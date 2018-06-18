893830ac526ffb5d6ef1530e30bcaf696adf7c0a
tommybuonomo <tommybuonomo@gmail.com>
2018-06-18 14:32:47 +0200

    [WIP] Start refactoring project structure for sample

---
+++ creative-viewpager-sample/src/main/java/com/tbuonomo/creativeviewpagersample/NatureItem.kt
@@ -0,0 +1,27 @@
+package com.tbuonomo.creativeviewpagersample
+
+import com.tbuonomo.tbd.R.drawable
+
+enum class NatureItem(open val userDrawable: Int, open val natureDrawable: Int) {
+  NATURE0(drawable.user_000, drawable.nature_000),
+  NATURE1(drawable.user_001, drawable.nature_001),
+  NATURE2(drawable.user_002, drawable.nature_002),
+  NATURE3(drawable.user_003, drawable.nature_003),
+  NATURE4(drawable.user_004, drawable.nature_004),
+  NATURE5(drawable.user_005, drawable.nature_005),
+  NATURE6(drawable.user_006, drawable.nature_006),
+  NATURE7(drawable.user_007, drawable.nature_007),
+  NATURE8(drawable.user_008, drawable.nature_008),
+  NATURE9(drawable.user_009, drawable.nature_009),
+  NATURE10(drawable.user_010, drawable.nature_010),
+  NATURE11(drawable.user_011, drawable.nature_011),
+  NATURE12(drawable.user_012, drawable.nature_012),
+  NATURE13(drawable.user_013, drawable.nature_013),
+  NATURE14(drawable.user_014, drawable.nature_014),
+  NATURE15(drawable.user_015, drawable.nature_015),
+  NATURE16(drawable.user_016, drawable.nature_016),
+  NATURE17(drawable.user_017, drawable.nature_017),
+  NATURE18(drawable.user_018, drawable.nature_018),
+  NATURE19(drawable.user_019, drawable.nature_019),
+  NATURE20(drawable.user_020, drawable.nature_020),
+}
